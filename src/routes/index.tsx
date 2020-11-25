e711f2e57e6efb08be473b930eb13adc8d3eafef
Douglas Gomes <douglasgomes.rr@gmail.com>
2020-11-25 12:37:16 -0400

    create Dashboard page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from '~/pages/Login';
+import Dashboard from '~/pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/dashboard" exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
