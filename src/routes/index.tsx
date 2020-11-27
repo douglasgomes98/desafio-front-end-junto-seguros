d1019f9ddbc774c55e29888b272f1487c0d7ef7e
Douglas Gomes <douglasgomes.rr@gmail.com>
2020-11-26 21:54:14 -0400

    create page details

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route';
 import Login from '~/pages/Login';
 import Movies from '~/pages/Movies';
 import MyList from '~/pages/MyList';
+import Details from '~/pages/Details';
 
 const Routes: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={Login} />
       <Route path="/movies" component={Movies} isPrivate />
       <Route path="/my_list" component={MyList} isPrivate />
+      <Route path="/details" component={Details} isPrivate />
     </Switch>
   );
 };
